f792e4faae21dbeae045ff1e9dfb9e8e6d17e0b7
williamroque <william.aroque@gmail.com>
2019-09-10 09:03:42 -0300

    Automatically adjusts PMT.

--- arc.py
+++ arc.py
@@ -88,55 +88,92 @@ amort_perc_sen_evol = []
 sub_finished = False
 sen_finished = False
 
-for m in range(len(months)):
-    if sub_finished:
-        break
+while True:
+    for m in range(len(months)):
+        if sub_finished:
+            break
 
-    juros_sub = saldo_sub * t_em_mensal
+        juros_sub = saldo_sub * t_em_mensal
 
-    if not sen_finished:
-        amort_sub = 0
+        if not sen_finished:
+            amort_sub = 0
 
-        juros_sen = saldo_sen * t_em_senior_mensal
+            juros_sen = saldo_sen * t_em_senior_mensal
 
-        if m > g_period - 1:
-            pmt_sub = juros_sub + despesas
-            pmt_sen = flux_total[m - 1] * pmt_proper - pmt_sub
-            amort_sen = pmt_sen - juros_sen
-            amort_perc_sen = amort_sen / saldo_sen
+            if m > g_period - 1:
+                pmt_sub = juros_sub + despesas
+                pmt_sen = flux_total[m - 1] * pmt_proper - pmt_sub
+                amort_sen = pmt_sen - juros_sen
+                amort_perc_sen = amort_sen / saldo_sen
+
+                if amort_sen < 0:
+                    break
+            else:
+                pmt_sub = pmt_sen = amort_sen = amort_perc_sen = 0
+
+            saldo_sen = saldo_sen + juros_sen - pmt_sen
         else:
-            pmt_sub = pmt_sen = amort_sen = amort_perc_sen = 0
+            juros_sen = pmt_sen = amort_sen = saldo_sen = 0
 
-        saldo_sen = saldo_sen + juros_sen - pmt_sen
+            pmt_sub = flux_total[m - 1] * pmt_proper
+            amort_sub = pmt_sub - juros_sub - despesas
+
+        amort_perc_sub = amort_sub / saldo_sub
+
+        if amort_perc_sen >= 1 and not sen_finished:
+            amort_sen = saldo_sen_evol[-1]
+            pmt_sen = amort_sen + juros_sen
+
+            pmt_sub = flux_total[m - 1] * pmt_proper - pmt_sen
+            amort_sub = pmt_sub - juros_sub - despesas
+
+            amort_perc_sub = amort_sub / saldo_sub
+
+            sen_finished = True
+
+        if amort_perc_sub >= 1:
+            amort_sub = saldo_sub_evol[-1]
+            pmt_sub = amort_sub + juros_sub + despesas
+            sub_finished = True
+
+        saldo_sub = saldo_sub + despesas + juros_sub - pmt_sub
+
+        saldo_sub_evol.append(max(0, saldo_sub))
+        despesas_sub_evol.append(despesas)
+        juros_sub_evol.append(juros_sub)
+        amort_sub_evol.append(amort_sub)
+        pmt_sub_evol.append(pmt_sub)
+        amort_perc_sub_evol.append(min(100, amort_perc_sub * 100))
+
+        saldo_sen_evol.append(max(0, saldo_sen))
+        juros_sen_evol.append(juros_sen)
+        amort_sen_evol.append(amort_sen)
+        pmt_sen_evol.append(pmt_sen)
+        amort_perc_sen_evol.append(min(100, amort_perc_sen * 100))
+
+    if not sub_finished:
+        saldo_sub = total * r_sub / 100
+        saldo_sen = total * r_sen / 100
+
+        saldo_sub_evol      = []
+        despesas_sub_evol   = []
+        juros_sub_evol      = []
+        amort_sub_evol      = []
+        pmt_sub_evol        = []
+        amort_perc_sub_evol = []
+
+        saldo_sen_evol      = []
+        juros_sen_evol      = []
+        amort_sen_evol      = []
+        pmt_sen_evol        = []
+        amort_perc_sen_evol = []
+
+        sub_finished = False
+        sen_finished = False
+
+        pmt_proper = int((pmt_proper + .01) * 100) / 100
     else:
-        juros_sen = pmt_sen = amort_sen = saldo_sen = 0
-
-        pmt_sub = flux_total[m - 1] * pmt_proper
-        amort_sub = pmt_sub - juros_sub - despesas
-
-    amort_perc_sub = amort_sub / saldo_sub
-
-    if amort_perc_sen >= 1 and not sen_finished:
-        pmt_sub = flux_total[m - 1] * pmt_proper - pmt_sen
-        sen_finished = True
-
-    if amort_perc_sub >= 1:
-        sub_finished = True
-
-    saldo_sub = saldo_sub + despesas + juros_sub - pmt_sub
-
-    saldo_sub_evol.append(max(0, saldo_sub))
-    despesas_sub_evol.append(despesas)
-    juros_sub_evol.append(juros_sub)
-    amort_sub_evol.append(amort_sub)
-    pmt_sub_evol.append(pmt_sub)
-    amort_perc_sub_evol.append(min(100, amort_perc_sub * 100))
-
-    saldo_sen_evol.append(saldo_sen)
-    juros_sen_evol.append(juros_sen)
-    amort_sen_evol.append(amort_sen)
-    pmt_sen_evol.append(pmt_sen)
-    amort_perc_sen_evol.append(min(100, amort_perc_sen * 100))
+        break
 
 def print_data():
     for i, m in enumerate(months):
